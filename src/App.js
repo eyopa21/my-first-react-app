587701505503667991791d15a6ca1c6baaf61f1d
joba <jobtennis21@gmail.com>
2023-12-16 22:44:46 +0300

    v-for

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expenses from './components/Expense/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
-
-const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -25,9 +23,16 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   const addExpenseHandler = (expense) => {
-   console.log("from root", expense)
+    console.log("from root", expense)
+    expenses.push(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
  }
   return (
     
